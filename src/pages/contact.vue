65a70aecc05b941086107b972835fe0c8ff519f8
Lloyd Atkinson <lloydjatkinson@gmail.com>
2019-09-11 22:47:20 +0100

    More form changes

--- src/pages/contact.vue
+++ src/pages/contact.vue
@@ -148,7 +148,7 @@ export default {
                     ...this.formData,
                 }),
             })
-                .then(() => this.$router.push('/contact'))
+                .then(() => this.$router.push('/'))
                 .catch(error => alert(error));
         },
     },
