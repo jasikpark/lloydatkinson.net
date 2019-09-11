5e72b50d07f1aa52906a779ed0dc30879bbc289c
Lloyd Atkinson <lloydjatkinson@gmail.com>
2019-09-12 00:22:25 +0100

    Thank you page

--- src/components/navigation-bar.vue
+++ src/components/navigation-bar.vue
@@ -2,7 +2,9 @@
     <nav class="flex items-center justify-between flex-wrap bg-gray-100 p-6">
         <div class="flex items-center flex-shrink-0 text-gray-700 mr-6">
             <!-- <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg> -->
-            <span class="font-semibold text-xl tracking-tight">Lloyd Atkinson</span>
+            <g-link to="/">
+                <span class="font-semibold text-xl tracking-tight hover:text-red-vibrant">Lloyd Atkinson</span>
+            </g-link>
         </div>
         <div class="block lg:hidden">
             <button class="flex items-center px-3 py-2 border rounded text-red-vibrant border-red-vibrant hover:text-red-600 hover:border-red-600">
@@ -41,10 +43,12 @@
                         class="mr-5 text-gray-800 hover:text-red-600 svg-inline--fa fa-github fa-w-16 fa-lg"
                         :icon="['fab', 'github']" />
                 </a>
-                <a href="https://github.com/lloydjatkinson">
+                <g-link to="/contact">
                     <font-awesome
                         class="text-gray-800 hover:text-red-600 svg-inline--fa fa-github fa-w-16 fa-lg"
                         :icon="['far', 'envelope']" />
+                </g-link>
+                <a href="https://github.com/lloydjatkinson">
                 </a>
             </div>
             <!-- <div>
---
+++ src/pages/thank-you.vue
@@ -0,0 +1,16 @@
+<template>
+  <layout>
+    <div class="mt-24 text-center align-middle justify-center">
+      <h1 class="text-4xl sm:text-6xl font-light leading-tight">Thank you!</h1>
+      <h2
+        class="mt-2 text-3xl sm:text-5xl text-gray-600 font-light leading-tight"
+      >I'll get back to you soon</h2>
+    </div>
+  </layout>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
