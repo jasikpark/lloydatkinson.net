7637f6cbe678b4a62be928c123af16fea0bbcfa2
Lloyd Atkinson <lloydjatkinson@gmail.com>
2019-09-14 10:50:39 +0100

    Google Analytics

--- gridsome.config.js
+++ gridsome.config.js
@@ -17,6 +17,12 @@ module.exports = {
         shouldImport: true,
         shouldTimeTravel: true,
       }
+    },
+    {
+      use: '@gridsome/plugin-google-analytics',
+      options: {
+        id: 'UA-148006071-1'
+      }
     }
   ]
 };
