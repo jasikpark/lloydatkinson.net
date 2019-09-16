5d3c40060251145bfa8312a12a29724046460240
Lloyd Atkinson <lloydjatkinson@gmail.com>
2019-09-16 22:40:19 +0100

    Change margin

--- src/components/navigation-bar.vue
+++ src/components/navigation-bar.vue
@@ -3,7 +3,7 @@
         <div class="flex items-center flex-shrink-0 text-gray-700 mr-6">
             <!-- <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg> -->
             <g-link to="/">
-                <span class="mr-8 font-semibold text-xl tracking-tight lg:hover:text-red-vibrant">Lloyd Atkinson</span>
+                <span class="mr-4 font-semibold text-xl tracking-tight lg:hover:text-red-vibrant">Lloyd Atkinson</span>
             </g-link>
         </div>
         <div class="block lg:hidden">
