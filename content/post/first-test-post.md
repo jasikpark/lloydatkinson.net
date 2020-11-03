82c8b18f03cc826fa48b1aa215ff7ed09eb20eb3
Lloyd Atkinson <lloydjatkinson@gmail.com>
2020-11-03 21:18:47 +0000

    Remove Gridsome totally

--- content/post/first-test-post.md
+++
@@ -1,8 +0,0 @@
----
-title: First test post
-published: false
----
-
-# Hello world!
-
-<feature-heading subtitle>Why does {1][1][1][1][2][][1][1] evalutate to 42 In Javascript?</feature-heading>
--- src/pages/404.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <layout>
-        <div class="mt-24 flex justify-center w-full">
-            <g-image src="~/assets/404.jpg" width="600" class="rounded-lg" />
-        </div>
-    </layout>
-</template>
