74c436784bd79008573d8e8128b2789e44062c77
Lloyd Atkinson <lloydjatkinson@gmail.com>
2020-11-06 00:42:38 +0000

    Add commit test

---
+++ pages/about.vue
@@ -0,0 +1,19 @@
+<template>
+    <p>{{ commit }}</p>
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({
+    data () {
+        return {
+            commit: process.env.COMMIT_REF
+        }
+    }
+})
+</script>
+
+<style>
+
+</style>
