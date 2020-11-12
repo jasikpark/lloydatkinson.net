0a67844471f9866c7a60cebf0a146578418e66c4
Lloyd Atkinson <lloydjatkinson@gmail.com>
2020-11-12 01:58:51 +0000

    Implement unit tests for NavigationLink.vue

--- components/layout/NavigationLink.spec.js
+++ components/layout/NavigationLink.spec.js
@@ -7,10 +7,10 @@ describe('layout', () => {
     describe('components', () => {
         describe('NavigationLink.vue', () => {
             const makeNuxtLink = (values = {}) => {
-                const defaultOpts = { navigate: jest.fn }
+                const defaultOptions = { navigate: jest.fn }
                 return {
                     render () {
-                        return this.$scopedSlots.default({ ...values, ...defaultOpts })
+                        return this.$scopedSlots.default({ ...values, ...defaultOptions })
                     }
                 }
             };
@@ -18,29 +18,89 @@ describe('layout', () => {
             it('should render a link to the given route and contain the default slot content', () => {
                 const wrapper = mount(NavigationLink, {
                     stubs: {
-                        'NuxtLink': makeNuxtLink({ isActive: true, href: '' })
+                        'NuxtLink': makeNuxtLink({ isActive: true, href: '/test' })
+                    },
+                    propsData: {
+                        to: '/test'
+                    },
+                });
+
+                expect(wrapper.text()).toBe('/test');
+            });
+
+            it('should render a link to the given route and contain the given slot content', () => {
+                const navigationLinkTest = 'Navigation Link';
+
+                const wrapper = mount(NavigationLink, {
+                    stubs: {
+                        'NuxtLink': makeNuxtLink({ isActive: true, href: '/test' })
                     },
                     propsData: {
                         to: '/test'
                     },
                     slots: {
-                        default: '<span>Hello</span>'
+                        default: navigationLinkTest,
                     }
                 });
 
-                console.log(wrapper.html())
-                expect(wrapper.text()).toBe('Hello');
+                expect(wrapper.text()).toBe(navigationLinkTest);
             });
 
-            it('should render a link to the given route and contain the given slot content', () => {
+            it('should return default active class from computed when active class prop is not set', () => {
                 const wrapper = mount(NavigationLink, {
                     stubs: {
-                        'NuxtLink': makeNuxtLink({ isActive: true, navigate: jest.fn, href: '' })
+                        'NuxtLink': makeNuxtLink({ isActive: true, href: '/test' })
                     },
                     propsData: {
                         to: '/test'
-                    }
-                })
+                    },
+                });
+
+                expect(wrapper.vm.style.active).toBe('text-black');
+            });
+
+            it('should return specified active class from computed when active class prop is set', () => {
+                const activeClass = 'active class';
+                const wrapper = mount(NavigationLink, {
+                    stubs: {
+                        'NuxtLink': makeNuxtLink({ isActive: true, href: '/test' })
+                    },
+                    propsData: {
+                        to: '/test',
+                        activeClass: activeClass
+                    },
+                });
+
+                expect(wrapper.vm.style.active).toBe(activeClass);
+            });
+
+            it('should return default inactive class from computed when inactive class prop is not set', () => {
+                const wrapper = mount(NavigationLink, {
+                    stubs: {
+                        'NuxtLink': makeNuxtLink({ isActive: true, href: '/test' })
+                    },
+                    propsData: {
+                        to: '/test'
+                    },
+                });
+
+                expect(wrapper.vm.style.inactive).toBe('text-gray-600');
+            });
+
+            it('should return specified inactive class from computed when inactive class prop is set', () => {
+                const inactiveClass = 'inactive class';
+
+                const wrapper = mount(NavigationLink, {
+                    stubs: {
+                        'NuxtLink': makeNuxtLink({ isActive: true, href: '/test' })
+                    },
+                    propsData: {
+                        to: '/test',
+                        inactiveClass: inactiveClass
+                    },
+                });
+
+                expect(wrapper.vm.style.inactive).toBe(inactiveClass);
             });
         });
     });
--- components/layout/NavigationLink.vue
+++ components/layout/NavigationLink.vue
@@ -5,9 +5,9 @@
         <a
             :href="href"
             class="cursor-pointer hover:text-gray-900"
-            :class="[ isActive ? 'text-black' : 'text-gray-600' ]"
+            :class="[ isActive ? style.active : style.inactive ]"
             @click="navigate">
-            <slot>{{ to }} {{ isActive }}</slot>
+            <slot>{{ to }}</slot>
         </a>
     </NuxtLink>
 </template>
@@ -21,7 +21,34 @@ export default {
             type: String,
             required: true,
             default: ''
+        },
+
+        activeClass: {
+            type: String,
+            required: false,
+            default: undefined,    
+        },
+
+        inactiveClass: {
+            type: String,
+            required: false,
+            default: undefined,    
+        },
+
+        hoverClass: {
+            type: String,
+            required: false,
+            default: undefined, 
         }
-    }
-}
+    },
+
+    computed: {
+        style () {
+            return {
+                active: this.activeClass || 'text-black',
+                inactive: this.inactiveClass || 'text-gray-600',
+            };
+        },
+    },
+};
 </script>
