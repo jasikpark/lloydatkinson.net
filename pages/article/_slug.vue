1dfa8bbba736bcdaa0a237b15bf04276921f47d5
Lloyd Atkinson <lloydjatkinson@gmail.com>
2021-01-12 01:23:30 +0000

    url test

---
+++ pages/article/_slug.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <article>
+            <nuxt-content
+                class="prose"
+                :document="article" />
+        </article>
+    </div>
+</template>
+
+<script>
+export default {
+    async asyncData ({ $content, params }) {
+        const article = await $content('articles', params.slug).fetch()
+
+        return { article }
+    }
+}
+</script>
