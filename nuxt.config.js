c1e40c2ec6740cacf5b4db786ca9a45d9eda6455
Lloyd Atkinson <lloydjatkinson@users.noreply.github.com>
2021-02-02 11:29:42 +0000

    Change progress bar colour

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,6 +22,10 @@ export default {
     // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
     plugins: [
     ],
+    
+    loading: {
+        color: 'red',
+    },
 
     // Auto import components (https://go.nuxtjs.dev/config-components)
     components: true,
